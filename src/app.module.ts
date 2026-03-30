53322916f153e9291eca04afec2d94a724e00463
DanYunak <danyloyunak2006@gmail.com>
2026-03-30 15:39:00 +0300

    feat: setup core modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
+import { DeliveriesModule } from './deliveries/deliveries.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, OrdersModule, DeliveriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
